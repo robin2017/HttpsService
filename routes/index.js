3ca081c1d5a2aa1bf0100fc6f6e2f5d2be4dc713
robin2017 <robin.seu@foxmail.com>
2019-06-16 21:37:02 +0800

    完成代码

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.protocol==='https'){
+      res.render('index', { title: 'Express/https' });
+  }else{
+      res.render('index', { title: 'Express/http' });
+  }
+
 });
 
 module.exports = router;
