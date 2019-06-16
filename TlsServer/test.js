0346898fbf3283728352d01d5be9d127ae6e63a4
robin2017 <robin.seu@foxmail.com>
2019-06-17 01:10:24 +0800

    tmp

---
+++ TlsServer/test.js
@@ -0,0 +1,10 @@
+var https = require('https');
+var fs = require('fs');
+var options = {
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./server.crt')
+};
+https.createServer(options, function(req, res) {
+    res.writeHead(200);
+    res.end('hello https');
+}).listen(8866);
